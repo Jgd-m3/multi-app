667d6b0a10dc488616501e9d7f55eb1e610b6927
gonzajai <javier.gonzalez@adidas.com>
2020-02-24 21:11:23 +0100

    adding new message

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Fib Calculator version KUBERNETES!!</h1>
+            <h1 className="App-title">MIRA QUE GUAPA QUE ES LA ELI!!!</h1>
             <br />
               <div>
                 <Link to="/">Home</Link>
